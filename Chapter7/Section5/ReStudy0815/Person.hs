a3ac272497e1e6a3728de8faf84b2865a4bda0e7
ndaDayo <hayato711+nda@gmail.com>
2022-08-15 22:32:08 +0900

    Person Eq

---
+++ Chapter7/Section5/ReStudy0815/Person.hs
@@ -0,0 +1,6 @@
+data Person = Person {firstName :: String,
+                      lastName :: String,
+                       age :: Int} deriving (Eq)
+
+nda = Person {firstName = "nda", lastName = "suga", age = 34}
+hayato = Person {firstName = "nda", lastName = "suga", age = 34}
