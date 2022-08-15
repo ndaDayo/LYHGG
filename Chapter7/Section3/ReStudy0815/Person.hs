d37a24451b29210d79ceaa16d93a9816452efedb
ndaDayo <hayato711+nda@gmail.com>
2022-08-15 21:58:14 +0900

    レコード構文を使わないとこうなる

--- Chapter7/Section3/ReStudy0815/Person.hs
+++ Chapter7/Section3/ReStudy0815/Person.hs
@@ -1,1 +1,7 @@
 data Person = Person String String Int Float String String deriving (Show)
+
+firstName :: Person -> String
+firstName (Person firstName _ _ _ _ _ ) = firstName
+
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ number _) = number
